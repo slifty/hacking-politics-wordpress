c76e3568f85de9515622ffe865cd53c0caa4448d
Dan Schultz <slifty@gmail.com>
2016-07-15 15:07:30 -0400

    Bootstrap added
    Also fixed an issue where the footer didn't load the wp_footer

---
+++ functions.php
@@ -0,0 +1,8 @@
+<?php
+
+function load_politics_theme_resources() {
+    wp_enqueue_style('bootstrap', get_template_directory_uri() . '/css/bootstrap.min.css');
+    wp_enqueue_style('style', get_stylesheet_uri());
+    wp_enqueue_script( 'bootstrap-js', get_template_directory_uri() . '/js/lib/bootstrap.min.js', array('jquery'), '2.3.2', true );
+}
+add_action('wp_enqueue_scripts', 'load_politics_theme_resources');
