b6cd877b14777840835e17304ba7a5218b247561
Dan Schultz <slifty@gmail.com>
2016-09-16 21:04:37 -0400

    Replaced signup form with Chimpy
    Unfortunately this now reqires a new plugin.  NOTE: we should
    add a wrapper around the join code to not error if chimpy
    is not installed.

--- page-join.php
+++ page-join.php
@@ -1,88 +1,28 @@
 <?php get_header(); ?>
 
 <div class="container">
-
 	<div class="page-header">
-
-		<div class="row">
-          <div class="col-lg-10 col-md-10 col-sm-10">
-            <h1>Join</h1>
-            <p class="lead">Sign up for Spin Time TV so you can remix election 2016.</span></p>
-            <p>Spin Time TV kicks off September 26th, 2016. It's free. We'll email you about once a week so you can follow along and participate. </p>
-          </div>
+        <div class="row">
+            <div class="col-lg-10 col-md-10 col-sm-10">
+                <h1>Join</h1>
+                <p class="lead">Sign up for Spin Time TV so you can remix election 2016.</p>
+                <p>Spin Time TV kicks off September 26th, 2016. It's free. We'll email you about once a week so you can follow along and participate. </p>
+            </div>
+        </div>
     </div>
-  </div>
     <div class="row">
 
 		<div class="col-lg-8">
-            			
             <div class="well bs-component">
-                    <!-- Begin MailChimp Signup Form -->
-                <div id="mc_embed_signup">
-                <form action="//spintime.us1.list-manage.com/subscribe/post?u=08ceeb43da0cdd92c6e3bf38e&amp;id=3c5946a379" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate" target="_blank" class="form-horizontal" novalidate>
-                    <div id="mc_embed_signup_scroll">
-                  
-                <div class="mc-field-group">
-                  <label for="mce-EMAIL" class="control-label">Email Address  <span class="asterisk">*</span>
-                </label>
-                  <input type="email" value="" name="EMAIL" class="required email form-control" id="mce-EMAIL">
-                </div>
-                <div class="mc-field-group">
-                  <label for="mce-FNAME">First Name  <span class="asterisk">*</span>
-                </label>
-                  <input type="text" value="" name="FNAME" class="required form-control" id="mce-FNAME">
-                </div>
-                <div class="mc-field-group">
-                  <label for="mce-LNAME">Last Name  <span class="asterisk">*</span>
-                </label>
-                  <input type="text" value="" name="LNAME" class="required form-control" id="mce-LNAME">
-                </div>
-                <div class="mc-field-group">
-                  <label for="mce-MMERGE3">Zip Code </label>
-                  <input type="text" value="" name="MMERGE3" class="form-control" id="mce-MMERGE3">
-                </div>
-                <div class="mc-field-group">
-                  <label for="mce-MMERGE5">Affiliation </label>
-                  <select name="MMERGE5" class="form-control" id="mce-MMERGE5">
-                  <option value=""></option>
-                  <option value="Journalist">Journalist</option>
-                <option value="Citizen">Citizen</option>
-                <option value="Student">Student</option>
-                <option value="Troll">Troll</option>
-                <option value="Educator">Educator</option>
-                <option value="Politico">Politico</option>
-                <option value="Mediamaker">Mediamaker</option>
-                <option value="Curious">Curious</option>
-                <option value="Other">Other</option>
-
-                  </select>
-                </div>
-                <div class="mc-field-group">
-                  <label for="mce-MMERGE6">Twitter </label>
-                  <input type="text" value="" name="MMERGE6" class="form-control" id="mce-MMERGE6">
-                </div>
-                  <div id="mce-responses" class="clear">
-                    <div class="response" id="mce-error-response" style="display:none"></div>
-                    <div class="response" id="mce-success-response" style="display:none"></div>
-                  </div>    <!-- real people should not fill this in and expect good things - do not remove this or risk form bot signups-->
-                    <div style="position: absolute; left: -5000px;" aria-hidden="true"><input type="text" name="b_08ceeb43da0cdd92c6e3bf38e_3c5946a379" tabindex="-1" value=""></div>
-                    <br>
-                    <div class="clear"><input type="submit" value="Submit" name="subscribe" id="mc-embedded-subscribe" class="btn btn-primary"></div>
-                    </div>
-                </form>
+                <?php chimpy_form(); ?>
             </div>
-
-<!--End mc_embed_signup-->
-
-          </div>          
         </div>
-    <div class="col-lg-4">
-                <a class="twitter-timeline"  href="https://twitter.com/hashtag/spintimetv" data-widget-id="757742999103213568">#spintimetv Tweets</a>
+        <div class="col-lg-4">
+            <a class="twitter-timeline"  href="https://twitter.com/hashtag/spintimetv" data-widget-id="757742999103213568">#spintimetv Tweets</a>
             <script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+"://platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);}}(document,"script","twitter-wjs");</script>
+        </div>
     </div>
-	</div>
-
 </div>
-  
+
 
 <?php get_footer(); ?>
