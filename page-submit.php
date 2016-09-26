9e0f83a86177b6e74e469336a08c176b0929f544
Dan Schultz <slifty@gmail.com>
2016-09-26 14:42:21 -0400

    Submission form (uses a plugin)
    The plugin used is AccessPress Anonymous Post Pro

---
+++ page-submit.php
@@ -0,0 +1,16 @@
+<?php get_header(); ?>
+
+<div class="container">
+
+    <div class="page-header">
+
+        <div class="row">
+
+            <div class="col-xs-12" style="background-color:white;">
+                <?php echo do_shortcode('[ap-form]');?>
+            </div>
+        </div>
+    </div>
+</div>
+
+<?php get_footer(); ?>
