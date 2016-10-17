29ee0ba5c755d09c1d06561f94b1dcd6336518c4
Dan Schultz <slifty@gmail.com>
2016-10-16 22:48:07 -0400

    Added more submission types to the embed logic

--- single-submission.php
+++ single-submission.php
@@ -9,7 +9,6 @@
                 the_post();
                 $post_id = get_the_ID();
                 $post_metadata = get_fields();
-                print_r($post_metadata);
                 ?>
                 <div class="row">
                     <div class="col-xs-12">
@@ -31,9 +30,35 @@
                                 ?>
                                 <iframe width="100%" height="400" src="<?php echo($url); ?>" frameborder="0" allowfullscreen></iframe>
                                 <?php
+                            } else if(strpos($url, "youtu.be") !== false) {
+                                // Make sure it's set up for embed
+                                if(strpos($url, "embed") === false) {
+                                    $id = strrchr($url, "/");
+                                    $url = "https://youtube.com/embed".$id;
+                                }
+                                ?>
+                                <iframe width="100%" height="400" src="<?php echo($url); ?>" frameborder="0" allowfullscreen></iframe>
+                                <?php
+                            } else if(strpos($url, "archive.org") !== false) {
+                                // Make sure it's set up for embed
+                                if(strpos($url, "embed") === false) {
+                                    $id = substr(strrchr($url, "/"), 1);
+                                    $url = "https://archive.org/pop/embed.html?debug=1&savedDataUrl=https%3A%2F%2Farchive.org%2Fservices%2Fmaker.php%3Fedl%3D".$id;
+                                }
+                                ?>
+                                <iframe src='<?php echo($url); ?>' width='640' height='403' frameborder='0' mozallowfullscreen webkitallowfullscreen allowfullscreen></iframe>
+                                <?php
+                            } else if(strpos($url, "vimeo.com") !== false) {
+                                if(strpos($url, "video") === false) {
+                                    $id = strrchr($url, "/");
+                                    $url = "https://player.vimeo.com/video".$id;
+                                }
+                                ?>
+                                <iframe src="<?php echo($url); ?>" width="640" height="360" frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe>
+                                <?php
                             } else {
                                 ?>
-                                <a href="<?php echo("url");?>"><?php echo("url");?></a>
+                                <a href="<?php echo($url);?>"><?php echo($url);?></a>
                                 <?php
                             }
                         ?>
