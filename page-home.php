c992f93c03ecf3d2871b0f5d78bdcfe7dd0319e2
Ben Moskowitz <benrito@gmail.com>
2016-10-18 00:06:40 -0400

    Fix

--- page-home.php
+++ page-home.php
@@ -28,7 +28,11 @@
       <h2>Debates, Then and Now</h2>
 <center>      <a href="https://www.youtube.com/watch?v=xYrgx1G_CW4"><img src="<?php bloginfo('stylesheet_directory'); ?>/img/1clay.jpg"></a>
       <a href="https://www.youtube.com/watch?v=ipLoS44xfO4"><img src="<?php bloginfo('stylesheet_directory'); ?>/img/1aubrey.jpg"></a>
-</center>        
+</center>        <ul class="pager">
+          <li class="previous"><a href="http://spintime.tv/join">&larr; Sign up</a></li>
+          <li>Now: <a href="http://spintime.tv/join">Debates, Then and Now</a></li>
+          <li class="next disabled"><a href="#">Next week &rarr;</a></li>
+        </ul>
       </div>
       </div>
 
